9a08d3122196fb7286d37b9e4110573ea571e976
woshiabao <285820415@qq.com>
2021-08-31 21:59:02 +0800

    first commit

---
+++ git-demo01/src/main/java/GitTest.java
@@ -0,0 +1,17 @@
+/**
+ * @description:
+ * @author: jay
+ * @time: 2021/8/31
+ */
+
+/**
+ *@ClassName GitTest
+ *@Description TODO
+ *@Author jay
+ *@version 1.0.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
