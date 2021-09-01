eb79672d3fe937282bcf1764adba9c6642653b7a
woshiabao <285820415@qq.com>
2021-09-01 09:37:16 +0800

    third commit

--- git-demo01/src/main/java/GitTest.java
+++ git-demo01/src/main/java/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello github");
+        System.out.println("hello gitee");
     }
 }
